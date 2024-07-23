f4620c785e19f8059a6559e7fd4fd789bc28dd4a
Aldo Ortega González <aeledeo.9@gmail.com>
2024-07-22 20:57:07 -0600

    battery

---
+++ Lilygo TTGO Lora32/test/pt/pt.ino
@@ -0,0 +1,104 @@
+/*
+  LoRaNow Simple Node
+
+  This code sends message and listen expecting some valid message from the gateway 
+*/
+//OLED 
+#include <Adafruit_SSD1306.h>//biblioteca para controlar displays OLED
+#define SCREEN_HEIGHT 64//indica a altura do display em pixels
+#define SCREEN_WIDTH 128//indica o tamanho da largura do display em pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);// inicialização e comunicação I2C do display instace
+
+
+#include <LoRaNow.h>
+unsigned long id;
+//Sensor BME680 needs
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#include <bme68x.h>
+#include <bme68x_defs.h>
+Adafruit_BME680 bme; // instância do sensor BME
+float BME680temperatura,BME680pressao,BME680umidade;//declaração das variáveis que serão usadas para armazenar os dados dos sensores
+//All sensor needs
+String LoRaMessage = "";
+void setup() {
+  Serial.begin(115200);
+  Serial.println("LoRaNow Simple Node with OLED");
+  //inicializar o display OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true);
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("ID:");
+  int x=0, y=10, level=0;
+  display.drawRect(x, y, 30, 15, SSD1306_WHITE);
+  
+  // Dibujar el terminal positivo de la batería
+  display.fillRect(x + 30, y + 4, 2, 7, SSD1306_WHITE);
+
+  // Dibujar el nivel de carga
+  int fillWidth = (level * 28) / 100; // Calcular el ancho del nivel de carga
+  display.fillRect(x + 1, y + 1, fillWidth, 13, SSD1306_WHITE);
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    //while (1);
+  }
+
+  // LoRaNow.setFrequencyCN(); // Select the frequency 486.5 MHz - Used in China
+  // LoRaNow.setFrequencyEU(); // Select the frequency 868.3 MHz - Used in Europe
+  // LoRaNow.setFrequencyUS(); // Select the frequency 904.1 MHz - Used in USA, Canada and South America
+  // LoRaNow.setFrequencyAU(); // Select the frequency 917.0 MHz - Used in Australia, Brazil and Chile
+
+  // LoRaNow.setFrequency(frequency);
+  // LoRaNow.setSpreadingFactor(sf);
+  // LoRaNow.setPins(ss, dio0);
+
+  // LoRaNow.setPinsSPI(sck, miso, mosi, ss, dio0); // Only works with ESP32
+
+  if (!LoRaNow.begin()) {
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);
+  }
+  display.setCursor(18, 0);
+  id = LoRaNow.id();
+  display.print(id,HEX);
+  display.display();
+
+  LoRaNow.onMessage(onMessage);
+  LoRaNow.onSleep(onSleep);
+  
+}
+
+void loop() {
+  LoRaNow.loop();
+}
+
+void obtenerBMEleituras(){
+  BME680temperatura=bme.readTemperature();
+  BME680pressao=bme.readPressure()/100.0F;
+  BME680umidade=bme.readHumidity();
+}
+
+void onMessage(uint8_t *buffer, size_t size)
+{
+  Serial.println("Receive Message: ");
+  Serial.write(buffer, size);
+  Serial.println();
+  Serial.println();
+}
+
+void onSleep()
+{
+  Serial.println("Sleep");
+  delay(5000); // "kind of a sleep"
+  Serial.println("Send Message");
+  obtenerBMEleituras();
+  LoRaMessage =String(BME680temperatura)+"°C "+String(BME680umidade)+"% "+String(BME680pressao)+"hPA ";
+  LoRaNow.print(LoRaMessage);
+  LoRaNow.send();
+  Serial.print("send: "+LoRaMessage);
+}
